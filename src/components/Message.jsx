1102f932a34626ae4a675df7b842f98bb2e70ba3
JuampaVLB <juampablovillalba25@gmail.com>
2023-02-17 00:19:02 -0300

    update

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import styles from '../modules/Message.module.css'; 
+import React from "react";
+import styles from "../modules/Message.module.css";
 
 function Message(props) {
   return (
     <li>
-        <p className={
-          `${props.from === "Me" ? styles.Me : styles.Server}`
-          }>{props.from}: {props.message}</p>
+      <p>
+        <span className={`${props.from === "Me" ? styles.Me : styles.Server}`}>
+          {props.from}
+        </span>
+        : {props.message}
+      </p>
     </li>
-  )
+  );
 }
 
 export default Message;
